3e1673956063ec9730add47447a2d75704f15fd9
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-12 11:20:33 +0100

    chore: remove console logs from middlewares

--- middlewares/index.js
+++ middlewares/index.js
@@ -4,23 +4,19 @@ const actionModel = require('../data/helpers/actionModel');
 async function validateId(req, res, next) {
   const id = Number(req.params.id);
   const endpoint = req.originalUrl.split('/')[2];
-  console.log(id);
   if (Number.isNaN(id) || id % 1 !== 0 || id < 0) {
     return res.status(400).send({
       message: `invalid ${endpoint} id provided`,
     });
   }
-  console.log(endpoint);
   try {
     let db;
     if (endpoint === 'actions') {
-      console.log('yes');
       db = actionModel;
     } else {
       db = projectModel;
     }
     const data = await db.get(id);
-    console.log(data);
     if (!data) {
       return res.status(400).send({
         message: `${endpoint} id provided does not exist`,
